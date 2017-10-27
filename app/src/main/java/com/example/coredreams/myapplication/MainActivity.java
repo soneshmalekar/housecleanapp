fb46a5215c35eb884203b451ae88719bc9f2fd6c
sonesh malekar <sonesh@digital-designinc.com>
2017-10-27 16:55:06 +0545

    asd

---
+++ app/src/main/java/com/example/coredreams/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.coredreams.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    TextView textView;
+    Button login,createaccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login=(Button)findViewById(R.id.login);
+        createaccount=(Button)findViewById(R.id.createaccount);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);            }
+        });
+        createaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);            }
+        });
+
+
+    }
+}
